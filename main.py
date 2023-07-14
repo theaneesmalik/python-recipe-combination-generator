d51ffa5900d0a2f1af1cd58ab128787e281bc8e3
Anees Malik <theaneesmalik@gmail.com>
2023-07-14 20:30:19 +0500

    More generic code

--- main.py
+++ main.py
@@ -2,17 +2,19 @@ import re
 
 
 class Ingredient:
-    def __init__(self, name: str, amounts):
+    def __init__(self, name:str, amounts):
         self.name = name
         self.amounts = amounts
-        self.length = max(max(len(amount) for amount in amounts), len(name)) + 2
+        self.minLength = max(max(len(amount) for amount in amounts), len(name))
 
-    def get_header(self):
-        return f"{self.name.center(self.length,' ')}|"
-        # return f"{' ' * (self._spacing + self._extra)}{self.name}{' ' * self._spacing}|"
+    # def get_header(self ,align, filler, separator):
+    #     return f"{self.name.center(self.length, filler)}{separator}"
+    def get_header(self, left_padding, right_padding, padding_filler, separator='|'):
+        return f"{padding_filler * left_padding}{self.name.center(self.minLength, padding_filler)}{padding_filler * right_padding}{separator}"
 
-    def get_divider(self, filler):
-        return f"{''.center(self.length, filler)}|"
+
+    def get_divider(self, filler, left_padding, right_padding, separator):
+        return f"{filler * (self.minLength + left_padding + right_padding)}{separator}"
 
     def get_special_divider(self, filler):
         filler_length = len(filler)
@@ -21,28 +23,37 @@ class Ingredient:
         result = repeated_fillers + filler[:remaining_length]
         return result
 
+    def get_length(self, length):
+        if(length < self.minLength): return self.minLength
+        else: return length
 
 class Recipe:
     def __init__(self, name):
         self.name = name
         self.ingredients = []
+        self.combinations = []
 
     def add_ingredient(self, name, amounts):
         ingredient = Ingredient(name, amounts)
         self.ingredients.append(ingredient)
 
-    @property
-    def combinations(self):
+    def generate_combinations(self):
         combinations = [[]]
 
         for ingredient in self.ingredients:
             ingredient_combinations = []
+
             for combination in combinations:
                 for amount in ingredient.amounts:
                     new_combination = combination + [(ingredient.name, amount)]
+                    # new_combination.append((ingredient.name, amount))
                     ingredient_combinations.append(new_combination)
+
             combinations = ingredient_combinations
-        return combinations
+
+        self.combinations = combinations
+
+
 
 
 def extract_recipe_data(input_file):
@@ -71,28 +82,24 @@ def extract_recipe_data(input_file):
     return recipes
 
 
-def write_recipe_combinations(recipe):
+def write_recipe_combinations(recipe,first_column_name='Combos', divider_filler="-" ,separator='|',left_padding=0,right_padding=0,padding_filler=' '):
     output_file = f"{recipe.name}_recipe_combos.txt"
 
     with open(output_file, "w") as file:
-        header = f"|{'Combos'.center(8, ' ')}|"
-        divider1 = f"|{''.center(8,'-')}|"
-        divider2 = f"|{''.center(8,'*')}|"
-        divider3 = f"|{'*-' * 4}|"
+        header = f"{separator}{padding_filler * left_padding}{first_column_name}{padding_filler * right_padding}{separator}"
+        divider1 = f"{separator}{''.center(len(header) - 2,divider_filler)}{separator}"
 
         for ingredient in recipe.ingredients:
             ingredient_name = ingredient.name
-            header += ingredient.get_header()
-            divider1 += ingredient.get_divider("-")
-            divider2 += ingredient.get_divider("8")
-            divider3 += ingredient.get_special_divider("*-")
+            header += ingredient.get_header(left_padding, right_padding, padding_filler, separator)
+            divider1 += ingredient.get_divider(divider_filler,left_padding, right_padding, separator)
 
         # file.write(f"{divider3}\n")
         file.write(f"{header}\n")
         file.write(f"{divider1}\n")
 
         for index, combination in enumerate(recipe.combinations, 1):
-            row = f"|{str(index).center(8)}|"
+            row = f"{separator}{padding_filler * left_padding}{str(index).center(len(first_column_name),padding_filler)}{padding_filler * right_padding}{separator}"
             for ingredient_name, amount in combination:
                 current_ingredient = next(
                     (
@@ -103,25 +110,28 @@ def write_recipe_combinations(recipe):
                     None,
                 )
                 if current_ingredient:
-                    max_length = current_ingredient.length
-                    row += f"{amount.center(max_length)}|"
+                    max_length = current_ingredient.minLength
+                    row += f"{padding_filler * left_padding}{amount.center(max_length,padding_filler)}{padding_filler * right_padding}{separator}"
             file.write(f"{row}\n")
-            # file.write(f"{divider3}\n")
-
-            # if index % 2 == 0:
-            #     file.write(f"{divider1}\n")
-            # else:
-            #     file.write(f"{divider2}\n")
 
     print(f"Generated recipe combinations for {recipe.name} in {output_file}")
+
 
 
 def main():
     input_file = "recipes_input_data.txt"
     recipes = extract_recipe_data(input_file)
-
+    # Change below variables for custome variations
+    first_column_name='Combos'
+    divider_filler="-" 
+    separator='|'
+    left_padding=1
+    right_padding=1
+    padding_filler=' '
+    # Change above variables
     for recipe in recipes:
-        write_recipe_combinations(recipe)
+        recipe.generate_combinations()
+        write_recipe_combinations(recipe,first_column_name, divider_filler, separator, left_padding, right_padding, padding_filler)
 
 
 if __name__ == "__main__":
